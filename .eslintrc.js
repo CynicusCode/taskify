21bf534fadee4e8fbf51b87f0a52600a9ea5eb1d
CynicusCode <114907376+CynicusCode@users.noreply.github.com>
2024-02-01 12:02:11 -0600

    Clerkprovider set up correctly

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,40 +2,40 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true, // Added to cover both browser and Node.js environments
+    node: true // Added to cover both browser and Node.js environments
   },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
     "next", // Assuming usage of Next.js-specific linting configurations
-    "prettier", // Ensure Prettier compatibility
+    "prettier" // Ensure Prettier compatibility
   ],
   overrides: [
     {
       files: [".eslintrc.{js,cjs}"],
       parserOptions: {
-        sourceType: "script",
-      },
-    },
+        sourceType: "script"
+      }
+    }
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
-    sourceType: "module",
+    sourceType: "module"
   },
   plugins: [
     "@typescript-eslint",
     "react",
-    "prettier", // Added Prettier plugin
+    "prettier" // Added Prettier plugin
   ],
   rules: {
     "prettier/prettier": "error", // Enforce Prettier formatting as ESLint errors
-    "react/react-in-jsx-scope": "off", // Disable the rule requiring React to be in scope
+    "react/react-in-jsx-scope": "off" // Disable the rule requiring React to be in scope
   },
   settings: {
     react: {
-      version: "detect", // Automatically detect the React version
-    },
-  },
+      version: "detect" // Automatically detect the React version
+    }
+  }
 };
